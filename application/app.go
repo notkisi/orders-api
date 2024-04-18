e319f6a49bb6365a10401f60cab4e6d9445d6712
notkisi <kristijan4815@its.edu.rs>
2024-04-18 18:24:16 +0200

    Graceful shutdown complete

--- application/app.go
+++ application/app.go
@@ -2,17 +2,23 @@ package application
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
@@ -22,9 +28,39 @@ func (a *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
-	return nil
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("Failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server")
+
+	ch := make(chan error, 1)
+	go func() {
+		err = server.ListenAndServe()
+		if !errors.Is(err, http.ErrServerClosed) {
+			ch <- err
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		// cant use parent context as its channel Done is closed already at this point
+		// init shutdown with max 10s
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		fmt.Println("\nCTRL-C Caught. Initiating shutdown...")
+		return server.Shutdown(timeout)
+	}
 }
