7efc0e16a39341a79ea47d316e43b474252eb0af
notkisi <kristijan4815@its.edu.rs>
2024-04-18 17:29:36 +0200

    refactored code and routes

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/notkisi/orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Heartbeat("/ping"))
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
+	app := application.New()
 
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to start HTTP Server", err)
+		fmt.Println("failed to start app:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world\n"))
 }
